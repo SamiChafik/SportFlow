9a30e5f2dd0eb137c4bfb472087e537065bba52d
SamiChafik <sami.chafik00@gmail.com>
2025-03-14 14:52:21 +0000

    new constructer

--- src/main/java/com/example/sportflow/model/User.java
+++ src/main/java/com/example/sportflow/model/User.java
@@ -40,6 +40,12 @@ public class User {
         this.role = role;
     }
 
+    public User(int id, String first_name, String last_name) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
     public int getId() {
         return id;
     }
